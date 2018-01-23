ea21994f74b30259fed383e8774893b953897404
Samuel Dale <smhdale@live.com.au>
2018-01-23 14:46:41 +1000

    Server stuff

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 // Simple backend for cloudinary-browser.
 // Performs Cloudinary API requests and caches to a local DB.
 
+const path = require('path')
 const Express = require('express')
 const Datastore = require('nedb')
 const Cloudinary = require('cloudinary')
 
 // Set up database
 const DB_FILE = 'cache.db'
-const db = new Datastore({ filename: DB_FILE, autoload: true })
+const db = new Datastore({ filename: path.resolve(__dirname, DB_FILE), autoload: true })
 db.persistence.setAutocompactionInterval(360000)
 
 // Set up Cloudinary
