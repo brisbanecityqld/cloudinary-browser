842587a95d88a16199e90087dab02ac3313591c3
Samuel Dale <smhdale@live.com.au>
2018-01-23 15:10:05 +1000

    More server stuff
    Server not a viable approach based due to hosting requirements

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const Express = require('express')
 const Datastore = require('nedb')
 const Cloudinary = require('cloudinary')
 
+// Server settings
+const PORT = 8000
+
 // Set up database
 const DB_FILE = 'cache.db'
 const db = new Datastore({ filename: path.resolve(__dirname, DB_FILE), autoload: true })
@@ -14,3 +17,14 @@ db.persistence.setAutocompactionInterval(360000)
 // Set up Cloudinary
 const CONFIG = require('./config')
 Cloudinary.config(CONFIG)
+
+// Set up Express server
+const app = express()
+
+// Add endpoints
+app.get('/folder', (req, res) => {
+
+})
+
+// Start server
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
