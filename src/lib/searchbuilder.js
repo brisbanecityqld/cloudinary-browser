a630bf03e90f7be72f6b12006cc1174c2c27c4d3
Samuel Dale <smhdale@live.com.au>
2018-03-07 12:21:13 +1000

    Improved search again
    + Can now exclude terms with '-' modifier

--- src/lib/searchbuilder.js
+++ src/lib/searchbuilder.js
@@ -1,37 +1,53 @@
 // Constants
 const CH_QUOTE = '"'
-const CH_SPACE = ' '
+const CH_MINUS = '-'
+const CH_COMMA = ','
 const JOIN_OR = ' OR '
 const JOIN_AND = ' AND '
 
-// Removes unnecessary quotes from around a string
-// Then adds quotes if the string contains spaces
-// Suitable for use in Array.map() functions
-function fixQuotes (str) {
-  str = str.replace(/^"?(.*?)"?$/, '$1')
-  return str.indexOf(CH_SPACE) > -1
-    ? `"${str}"`
-    : str
-}
-
 // Takes a query and splits it into a space-delimited array, preserving quotes
 function splitQuery (query) {
-  return query.match(/(".*?"|[^ ]+)/g).map(fixQuotes)
+  return query.match(/([-]?".*?"|[^ ]+)/g)
+}
+
+// Gets the correct separator for a tag
+function getSep (tag) {
+  return tag.indexOf(CH_QUOTE) > -1 ? '=' : ':'
 }
 
 function makeTagSearch (terms) {
-  return terms.map(term => `tags:${term}`).join(JOIN_AND)
+  const arrPositives = terms.filter(t => t[0] !== CH_MINUS)
+  const arrNegatives = terms.filter(t => t[0] === CH_MINUS)
+
+  const positives = arrPositives.map(t => `tags${getSep(t)}${t}`).join(JOIN_AND)
+  const negatives = arrNegatives.map(t => t.slice(1)).join(CH_COMMA)
+
+  let out = ''
+  if (positives) {
+    out += positives
+  }
+  if (negatives) {
+    out += (out !== '' ? JOIN_AND : '') + `-tags:${negatives}`
+  }
+
+  return out
 }
 
 function makeFilenameSearch (terms) {
   // return terms.map(term => `filename${term.indexOf(CH_QUOTE) > -1 ? '=' : ':'}${term}`).join(JOIN_AND)
-  return terms.length === 1
-    ? `filename${terms[0].indexOf(CH_QUOTE) > -1 ? '=' : ':'}${terms[0]}`
-    : ''
+  const term = terms[0]
+  const sep = getSep(term)
+  return terms.length > 1
+    ? ''
+    : term.charAt(0) === CH_MINUS
+    ? `-filename${sep}${term.slice(1)}`
+    : `filename${sep}${term}`
 }
 
 // Takes a search query and creates a
 function createExpression (query) {
+  if (query.length === 0) return ''
+
   const terms = splitQuery(query.toLowerCase())
 
   // Search tags and filenames
